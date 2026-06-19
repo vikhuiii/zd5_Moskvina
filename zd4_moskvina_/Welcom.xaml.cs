5df18781956011e058ae510111f45286679c4080
vikhuiii <emoskvina867@gmail.com>
2026-06-19 05:40:43 +0500

    first

---
+++ zd4_moskvina_/Welcom.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace zd4_moskvina_
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Welcom : ContentPage
+	{
+		public Welcom ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
